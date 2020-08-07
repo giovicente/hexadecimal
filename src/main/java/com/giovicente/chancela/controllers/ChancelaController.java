5ee624779f75c15c285586d6f05657bf636d3ec0
Giovanni Vicente <gvn.gentile@gmail.com>
2020-08-07 10:31:24 -0300

    Adicionar limpeza de código e manter algoritmo de geração de um campo aleatório em hexadecimal ao invés de sha-1, que é um algoritmo mais pesado

--- src/main/java/com/giovicente/chancela/controllers/ChancelaController.java
+++ src/main/java/com/giovicente/chancela/controllers/ChancelaController.java
@@ -3,7 +3,7 @@ package com.giovicente.chancela.controllers;
 import com.giovicente.chancela.models.Chancela;
 import com.giovicente.chancela.services.ChancelaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +14,7 @@ public class ChancelaController {
     @Autowired
     private ChancelaService chancelaService;
 
-    @PostMapping
+    @GetMapping
     public Chancela postChancela() { return chancelaService.gerarChancela(); }
 
 }
--- src/main/java/com/giovicente/chancela/services/ChancelaService.java
+++ src/main/java/com/giovicente/chancela/services/ChancelaService.java
@@ -3,39 +3,33 @@ package com.giovicente.chancela.services;
 import com.giovicente.chancela.models.Chancela;
 import org.springframework.stereotype.Service;
 
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.time.LocalDate;
 import java.util.Random;
 
 @Service
 public class ChancelaService {
 
     private static final int QUANTIDADE_BLOCOS = 5;
-
     private Random random;
 
     public Chancela gerarChancela() {
 
+        String[] blocos = new String[QUANTIDADE_BLOCOS];
         Chancela chancela = new Chancela();
 
-        random = new Random();
-
-        LocalDate dataHoraTransacao = LocalDate.now();
-        String dataHoraString = dataHoraTransacao.toString() + random.nextInt();
-
-        try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-1");
-
-            digest.reset();
-            digest.update(dataHoraString.getBytes("utf8"));
-
-            chancela.setValor(String.format("%040x", new BigInteger(1, digest.digest())).toUpperCase());
-        // Incluir melhores logs e rastreabilidade do problema neste catch, para melhor análise em caso de erros em produção
-        } catch (Exception e) {
-            e.printStackTrace();
+        for (int i = 0; i < QUANTIDADE_BLOCOS; i++) {
+            blocos[i] = gerarBloco();
         }
 
+        chancela.setValor(blocos[0].concat(blocos[1]).concat(blocos[2]).concat(blocos[3]).concat(blocos[4]));
         return chancela;
     }
+
+    public String gerarBloco() {
+
+        random = new Random();
+        int valorAleatorio = random.nextInt();
+
+        return Integer.toHexString(valorAleatorio).toUpperCase();
+    }
+
 }
