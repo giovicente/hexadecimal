1450b41fe76555f7a47f8afb8785aea31d8eecbb
Giovanni Vicente <gvn.gentile@gmail.com>
2022-01-14 20:12:56 -0300

    Test push from intellij

--- src/main/java/com/giovicente/chancela/ChancelaApplication.java
+++ src/main/java/com/giovicente/chancela/ChancelaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChancelaApplication {
 
+	// Comment
 	public static void main(String[] args) {
 		SpringApplication.run(ChancelaApplication.class, args);
 	}
